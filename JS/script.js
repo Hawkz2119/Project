060b3034843c438c479a70c2b119ec8f5897d310
Dias <example@gmail.com>
2023-03-14 11:01:18 +0100

    first commit

---
+++ JS/script.js
@@ -0,0 +1,75 @@
+"use strict";
+/* console.log(1);
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+leftBorderWidth = 16;
+console.log(leftBorderWidth);
+const obj = {
+	a : 5
+};
+
+obj.a = 150;
+console.log(obj);
+
+console.log(name);
+var name = "Dias";
+
+
+// snake_case
+// camelCase
+//UPPER_SNAKE_CASE
+// kebab-case
+// PascalCase
+
+/*const obj = {
+    Name : "Dias",
+    Age : 22,
+    isMarried : false
+
+};
+console.log(obj.isMarried);
+
+let arr = ["apple.jpg", "banana.png", 6, "ada", { }, []];
+console.log(arr[1]);
+
+const arrObj = {
+    0: "a",
+    1: "b"
+};
+arrObj.a = "7232";
+console.log(arrObj["a"]);
+*/
+//const answer = confirm("Are u here?");
+//console.log(answer);
+
+/*const answer = +prompt("Are you 18 yrs old?", "18");
+console.log(answer + 5); */
+/*const ans = [];
+ans[0] = prompt("What is your name?", "");
+ans[1] = prompt("What is your surname?", "");
+ans[2] = prompt("What is your age?", "");
+
+document.write(ans);*/
+
+/*let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
+
+console.log(5%2)
+
+const isChecked = true,
+      isClosed = false;
+
+console.log(isChecked && isClosed);
+
+const isChecked = true,
+      isClosed = false;
+
+console.log(isChecked || isClosed);*/
